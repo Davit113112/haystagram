c1ea0f47bf379c7301a93639a13775bb1d809380
Davit113112 <57672191+Davit113112@users.noreply.github.com>
2021-03-26 13:47:14 -0700

    initial commit

--- haystagram.js
+++ haystagram.js
@@ -1,1 +1,27 @@
 
+express = require('express')
+const app = express()
+var router = express.Router()
+app.use('/', router)
+app.get('/', function (req, res) {
+         res.send('Hello World')
+})
+app.get('/users', function (req, res) {
+         res.send('Hello users');
+})
+app.get('/follow', function (req, res) {
+         res.send('Hello geri');
+})
+app.get('/photo', function (req, res) {
+         res.send('photo');
+})
+app.get('/story', function (req, res) {
+         res.send('story')
+});
+router.get('/like', function (req, res) {
+         res.send('mersi like i hamar')
+})
+router.get('/settings', function (req, res) {
+         res.send('mdar nastroika :D')
+})
+app.listen(3000)
